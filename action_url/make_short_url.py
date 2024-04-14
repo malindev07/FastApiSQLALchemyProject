9863c175e69b32f20d8150b9a4d26b1d07914d65
malindev07 <malindev07@yandex.ru>
2024-04-14 21:52:59 +0300

    improve mistakes 2

--- action_url/make_short_url.py
+++ action_url/make_short_url.py
@@ -1,6 +1,80 @@
+import random
+import re
 import pyshorteners
 
 
 def make_short_url(long_url: str):
-    short_url: pyshorteners.Shortener = pyshorteners.Shortener().tinyurl.short(long_url)
+    symbols_arr = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
+    ]
+
+    pattern = r"https://|http://"
+    match = re.search(pattern, long_url)
+
+    short_url: str = match[0]
+    for _ in range(10):
+        short_url += random.choice(symbols_arr)
+
+    # short_url: pyshorteners.Shortener = pyshorteners.Shortener().tinyurl.short(long_url)
     return short_url
